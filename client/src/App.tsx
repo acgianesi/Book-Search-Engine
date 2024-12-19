09b9a268f1cb74b239475d7dca1c2d80c9e52a5f
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-12-19 18:47:17 -0500

    debuggin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -26,10 +26,10 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
